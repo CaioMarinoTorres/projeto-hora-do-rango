7566c018ade4372ccf0b3d8cfffebb08de9e5853
CaioMarino <caiomt000@gmail.com>
2026-03-11 11:35:36 -0400

    commit inicial do projeto

---
+++ src/main/java/com/horadorango/projetohoradorango/ProjetoHoraDoRangoApplication.java
@@ -0,0 +1,13 @@
+package com.horadorango.projetohoradorango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoHoraDoRangoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoHoraDoRangoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/horadorango/projetohoradorango/ProjetoHoraDoRangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horadorango.projetohoradorango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoHoraDoRangoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
